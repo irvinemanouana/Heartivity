779aba646ba9bacbda2adaf84bfaa12a7ebbcd30
Soulier Antoine <soulierantoine@pc25.home>
2016-02-20 16:19:42 +0100

    Added several fields for profile view

--- Heartivity/views/ProfileViewController.h
+++ Heartivity/views/ProfileViewController.h
@@ -9,8 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface ProfileViewController : UIViewController
-@property (weak, nonatomic) IBOutlet UITextField *pseudo;
-
-
-
+    @property (weak, nonatomic) IBOutlet UISegmentedControl *segGender;
+    @property (weak, nonatomic) IBOutlet UITextField *email;
+    @property (weak, nonatomic) IBOutlet UITextField *pseudo;
+    @property (weak, nonatomic) IBOutlet UIDatePicker *date;
+    @property (weak, nonatomic) IBOutlet UITextField *weight;
+    @property (weak, nonatomic) IBOutlet UITextField *height;
 @end
--- Heartivity/views/ProfileViewController.m
+++ Heartivity/views/ProfileViewController.m
@@ -15,23 +15,36 @@
 @end
 
 @implementation ProfileViewController
+
+@synthesize segGender;
+@synthesize email;
 @synthesize pseudo;
+@synthesize date;
+@synthesize weight;
+@synthesize height;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     
     Session* session = [Session new];
     
-    /* if ([session sessionExist]==true) {
-     NSLog(@"%@",@"true");
-     }else{
-     NSLog(@"%@",@"false");
-     }*/
-    Person* p = [session getUserdata];
-    pseudo.text = p.pseudo;
-    NSLog(@"In ProfileViewController");
+    if ([session sessionExist] == true) {
+        Person* p = [session getUserdata];
+        email.text = p.email;
+        pseudo.text = p.pseudo;
+    	if ([p.gender isEqual: @"Homme"]) {
+            [segGender setSelectedSegmentIndex:0];
+        } else {
+            [segGender setSelectedSegmentIndex:1];
+        }
+        // date.date = p.birthday;
+
+        NSNumberFormatter *f = [NSNumberFormatter new];
+        f.numberStyle = NSNumberFormatterDecimalStyle;
     
-    //labelgender.text = p.gender;
+        weight.text = [p.weight stringValue];
+        height.text = [p.height stringValue];
+    }
     
     // Do any additional setup after loading the view from its nib.
 }
